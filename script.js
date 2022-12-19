7e248311f51bf8021ba74f820c618ca2202938c1
node777 <codefreak100101@gmail.com>
2022-12-19 19:35:07 +0100

    created ibuswap interface

---
+++ script.js
@@ -0,0 +1,349 @@
+
+  let sidebar=`
+  <a href="./index.html" target="_blank">Home</a>
+  <a href="./swap.html" target="_blank">Swap</a>
+  <a href="./swap.html" target="_blank">Wallet</a>
+  <div class="br"></div>
+  <a href="https://www.leviathanlobstergod.com/" target="_blank">Leviathan Lobster Sermons</a>
+  <a href="https://www.leviathanlobstergod.com/" target="_blank">Holy Order of the Claw</a>
+  <a href="https://holyorderoftheclaw.com/" target="_blank">Holy order Docs</a>
+  <a href="https://www.facebook.com/groups/560665388170838/" target="_blank">Facebook</a>
+  <a href="https://twitter.com/llgllc" target="_blank">Twitter</a>
+  <a href="https://en.wikipedia.org/wiki/Lobster" target="_blank">Discord</a>
+  
+  <div class="br"></div>
+  <a href="https://untamedanimals.com/how-do-lobsters-communicate-can-they-scream/" target="_blank">How do we do it?</a>
+
+  <a href="https://image.shutterstock.com/image-illustration/red-green-growth-graph-600w-47532643.jpg" target="_blank">Financial Model</a>
+  <a href="https://image.shutterstock.com/image-illustration/red-green-growth-graph-600w-47532643.jpg" target="_blank">Charts</a>
+  <a href="https://en.wikipedia.org/wiki/Lobster" target="_blank">Learn More</a>
+  <a href="mailto:contact@lorb.net">Contact Us</a>
+  <a href="https://www.facebook.com/groups/560665388170838/" target="_blank">About Us</a>`
+  document.getElementById("sidebar").innerHTML=sidebar
+  function displaySidebar(){
+      document.getElementById("ui").style="display:initial"
+  }
+  function hideSidebar(){
+      document.getElementById("ui").style="display:none"
+  }
+  let lob2=`
+
+                      ▒▒██▓▓▓▓▓▓▓▓████▒▒░░                                                                                      !
+        ▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██████████▒▒                                                                                !
+      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████▒▒                            ▓▓                                              !
+      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██░░              ▒▒          ▓▓                                              !
+    ▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓        ▒▒        ▓▓                                              !
+    ▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒  ██▓▓▒▒      ▓▓      ▒▒▒▒                                              !
+  ▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░    ▒▒▒▒    ▒▒      ▒▒          ░░▒▒▓▓░░                              !
+  ▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓      ▒▒▒▒  ▒▒      ▒▒        ▒▒▒▒    ░░                              !
+  ▓▓▓▓▓▓▓▓▓▓▓▓▒▒  ▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓      ▓▓▒▒▓▓▓▓    ▒▒      ▒▒                                        !
+  ▓▓▓▓▓▓▒▒  ░░▒▒▓▓▓▓▒▒▒▒▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒    ▒▒▓▓░░▒▒  ░░▓▓▒▒░░▓▓▒▒                                        !
+  ▓▓░░  ░░▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░    ▒▒▓▓▓▓▓▓▓▓      ▒▒  ▓▓▒▒▒▒▓▓  ░░▒▒                              ░░▓▓▓▓░░    !
+    ▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▒▒▓▓▒▒          ▒▒▓▓▓▓▓▓▓▓  ░░▓▓▒▒▒▒▓▓  ▓▓  ▓▓░░                              ▓▓▓▓▒▒▓▓    !
+      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░                      ▒▒▓▓▒▒▓▓▓▓▓▓  ▒▒▓▓  ▓▓  ▓▓  ▓▓                              ▓▓▓▓▒▒▓▓▒▒▒▒  !
+                                                  ▓▓▓▓▓▓██░░░░██▒▒██░░██░░▓▓                            ▒▒▒▒▒▒▓▓▓▓▓▓██  !
+                                                ░░▒▒██████▓▓▓▓▓▓▓▓▓▓██▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░    ▓▓▓▓▓▓▓▓▒▒▒▒▓▓  !
+                                              ▒▒  ▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▓▓░░!
+                                      ░░░░  ▓▓▒▒▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓░░!
+                                          ▒▒░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▓▓▓▓!
+                                          ▒▒▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓!
+                                          ░░░░▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓░░!
+                                      ░░░░  ▓▓▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▓▓▓▓░░!
+                                              ▓▓    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒  !
+                                                ░░▓▓▓▓████▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓      ░░▒▒  ░░░░  ░░          ▒▒▒▒▒▒▒▒▒▒▒▒▓▓░░!
+                                                ▓▓▓▓▓▓▓▓▒▒  ░░▓▓  ▓▓  ▓▓░░▓▓                            ▒▒░░▒▒▒▒▒▒▓▓▒▒  !
+    ░░░░                                      ▓▓▓▓▓▓▓▓▓▓▒▒  ▒▒▓▓  ▓▓  ▓▓  ▒▒                              ▒▒░░▒▒▒▒▒▒▒▒  !
+    ▒▒▒▒▓▓▓▓▓▓▓▓▓▓██▒▒                        ▒▒▓▓▒▒▓▓░░    ▓▓▒▒▒▒▓▓  ▓▓  ▓▓░░                            ░░▒▒▒▒▒▒▓▓    !
+  ▓▓▒▒▓▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▒▒        ▒▒▒▒▓▓▓▓▓▓      ▒▒  ▓▓▓▓▒▒▓▓  ░░▒▒                                ▒▒▒▒      !
+  ▒▒▒▒▓▓▒▒▓▓▒▒▒▒▒▒▒▒▒▒▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▒▒▓▓▓▓▓▓      ░░▒▒  ▒▒░░░░▓▓▒▒  ▓▓░░                                        !
+  ▓▓▒▒▒▒▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▒▒▒▒▓▓▒▒      ▓▓▓▓▒▒▒▒  ░░▒▒  ░░▓▓▓▓▒▒░░░░                                  !
+  ░░▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓        ▒▒░░▒▒▒▒    ▒▒      ▒▒░░                                      !
+    ▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▓▓▓▓▒▒▓▓▓▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓        ▒▒▒▒  ▓▓      ▓▓        ▒▒▒▒░░  ▓▓                              !
+    ▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▓▓▒▒▓▓▓▓      ░░▒▒▒▒    ▒▒      ▒▒░░          ▒▒▓▓                                !
+      ▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ██▓▓▒▒      ▓▓      ▒▒▒▒                                              !
+        ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██      ░░░░      ░░▒▒        ▓▓                                              !
+        ░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██                  ▒▒          ▓▓░░                                            !
+            ░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓▓▓                                  ░░                                              !
+                ░░▓▓▓▓██▓▓▓▓▓▓██▓▓                                                                                      !
+                `
+        let lob1=`
+      ████████                                                                                  
+      ██░░░░░░████                                                        ▓▓▓▓▓▓                
+        ████▒▒▒▒░░████                                              ▓▓▓▓▓▓                      
+            ██████▒▒▒▒▓▓██                                  ▒▒▓▓▓▓▒▒                            
+          ██░░▒▒████▒▒▒▒░░██                          ▓▓▓▓▓▓                                    
+          ██████    ██▓▓▒▒▓▓██                    ░░▒▒  ░░░░                                    
+                      ██▓▓▒▒░░██              ▒▒▒▒                                              
+    ██████████          ██████░░██        ▓▓▓▓                                                  
+    ██░░░░░░░░████          ██░░░░██  ▓▓▓▓                                                      
+      ██████▒▒░░░░██    ██████▒▒▒▒██▓▓                                                          
+            ██▒▒▒▒▒▒██  ██░░░░██████                                                            
+████████████████████████  ██▒▒░░░░░░██████                    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓          
+██▒▒▒▒░░░░░░░░░░░░░░░░░░██████▒▒██░░▒▒▒▒░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                          ██▓▓▓▓  
+  ██▒▒░░░░░░░░░░░░░░░░░░▒▒▒▒████▒▒░░▓▓▓▓▓▓▒▒██                      ██████████████████▓▓▒▒░░░░██
+    ████▒▒▒▒░░░░░░░░▒▒▒▒▒▒██░░░░██▒▒░░░░░░░░░░████        ██████████░░██░░░░░░██░░░░░░██████▒▒██
+        ██████▒▒▒▒▒▒▒▒▒▒██████▒▒██████▒▒░░████░░░░████████░░░░░░██░░░░██░░░░██░░░░░░▒▒██▒▒░░██  
+              ██████████    ░░██▒▒▒▒▒▒████░░░░░░░░██░░██░░██░░░░██░░██░░░░▒▒██▒▒▒▒▒▒▒▒████▒▒░░██
+                              ██▒▒▒▒██▒▒████▒▒▒▒██░░░░██░░██▒▒██▒▒▒▒██▒▒▒▒██▒▒▒▒██████▒▒░░██████
+                                ████▒▒██▒▒▒▒████████▒▒██▒▒▒▒████████████████████▒▒░░████▒▒▒▒██  
+                                ██▒▒██████▒▒▒▒▒▒▒▒▒▒████▒▒██▒▒██▒▒██▒▒▒▒██▒▒▒▒████████  ██████  
+                                  ██░░░░██████▒▒▒▒████▒▒████░░██░░░░██░░░░██████                
+                                ██░░████  ██░░████░░██░░██  ██  ████  ██████                    
+                              ██░░██      ██░░████░░████▒▒██                                    
+                            ██░░██    ████░░██  ██░░████░░██                                    
+                          ██▒▒██    ██░░▒▒██    ██░░██  ██░░████                                
+                        ██▓▓██    ██▒▒████      ██▒▒██    ██▓▓▒▒██████                          
+                        ████    ██░░██            ██▒▒████  ████▒▒▒▒▒▒██                        
+                              ██▒▒██                ██░░▒▒██    ████████                        
+                            ██▒▒██                    ████▒▒██                                  
+                            ████                          ████                                  
+                            `
+// for(c in lob1){document.getElementById("lobZone").innerHTML+=(lob1[c]==" ")?"&nbsp":(lob1[c]=="!"?"<br>":lob1[c])}
+//for(c in lob2){document.getElementById("lobZone").innerHTML+=(lob2[c]==" ")?"&nbsp":(lob2[c]=="!"?"<br>":lob2[c])}
+
+
+console.log(lob1)
+
+//document.getElementById("lobZone").innerHTML=lob1
+// lobZone             
+//let priceData=await fetch("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48&toTokenAddress=0x5c173a51468694c0114aad0c14cbea350f40c33e&amount=1000000")
+//priceData=await priceData.json()
+//console.log(priceData.toTokenAmount)
+
+const rpcUrls = {
+  ethereum: 'https://mainnet.infura.io/v3/6cac2ad604be45b298c9af77e9112e3e',
+  polygon: 'https://polygon.infura.io',
+  xdai: 'https://xdai.infura.io'
+}
+
+const slugToChainId = {
+  ethereum: 1,
+  polygon: 137,
+  xdai: 100
+}
+
+const tokenDecimals = {
+  USDC: 6,
+  ETH: 18
+}
+
+const addresses = {
+  ethereum: {
+    USDC: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    ETH: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+  },
+  polygon: {
+    USDC: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+    ETH: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
+  },
+  xdai: {
+    USDC: '0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83',
+    ETH: '0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1'
+  }
+}
+
+class OneInch {
+  constructor () {
+    this.baseUrl = 'https://api.1inch.exchange/v4.0'
+  }
+
+  async getQuote (config) {
+    const { chainId, fromTokenAddress, toTokenAddress, amount } = config
+    if (!chainId) {
+      throw new Error('chainId is required')
+    }
+    if (!fromTokenAddress) {
+      throw new Error('fromTokenAddrss is required')
+    }
+    if (!toTokenAddress) {
+      throw new Error('toTokenAddress is required')
+    }
+    if (!amount) {
+      throw new Error('amount is required')
+    }
+    const url = `${this.baseUrl}/${chainId}/quote?fromTokenAddress=${fromTokenAddress}&toTokenAddress=${toTokenAddress}&amount=${amount}`
+    const result = await this.getJson(url)
+    if (!result.toTokenAmount) {
+      console.log(result)
+      throw new Error('expected tx data')
+    }
+
+    const { toTokenAmount } = result
+
+    return toTokenAmount
+  }
+
+  async getAllowance (config) {
+    const { chainId, tokenAddress, walletAddress } = config
+    if (!chainId) {
+      throw new Error('chainId is required')
+    }
+    if (!tokenAddress) {
+      throw new Error('tokenAddress required')
+    }
+    if (!walletAddress) {
+      throw new Error('walletAddress is required')
+    }
+
+    const url = `${this.baseUrl}/${chainId}/approve/allowance?tokenAddress=${tokenAddress}&walletAddress=${walletAddress}`
+    const result = await this.getJson(url)
+    if (result.allowance === undefined) {
+      console.log(result)
+      throw new Error('expected tx data')
+    }
+
+    return result.allowance
+  }
+
+  async getApproveTx (config) {
+    const { chainId, tokenAddress, amount } = config
+    if (!chainId) {
+      throw new Error('chainId is required')
+    }
+    if (!tokenAddress) {
+      throw new Error('tokenAddress required')
+    }
+    if (!amount) {
+      throw new Error('amount is required')
+    }
+
+    const url = `${this.baseUrl}/${chainId}/approve/transaction?&amount=${amount}&tokenAddress=${tokenAddress}`
+    const result = await this.getJson(url)
+    if (!result.data) {
+      console.log(result)
+      throw new Error('expected tx data')
+    }
+
+    const { data, to, value } = result
+
+    return {
+      data,
+      to,
+      value
+    }
+  }
+
+  async getSwapTx (config) {
+    const { chainId, fromTokenAddress, toTokenAddress, fromAddress, amount, slippage } = config
+    if (!chainId) {
+      throw new Error('chainId is required')
+    }
+    if (!fromTokenAddress) {
+      throw new Error('fromTokenAddrss is required')
+    }
+    if (!toTokenAddress) {
+      throw new Error('toTokenAddress is required')
+    }
+    if (!fromAddress) {
+      throw new Error('fromAddress is required')
+    }
+    if (!amount) {
+      throw new Error('amount is required')
+    }
+    if (!slippage) {
+      throw new Error('slippage is required')
+    }
+    const url = `${this.baseUrl}/${chainId}/swap?fromTokenAddress=${fromTokenAddress}&toTokenAddress=${toTokenAddress}&amount=${amount}&fromAddress=${fromAddress}&slippage=${slippage}`
+    const result = await this.getJson(url)
+    if (!result.tx) {
+      console.log(result)
+      throw new Error('expected tx data')
+    }
+
+    const { data, to, value } = result.tx
+
+    return {
+      data,
+      to,
+      value
+    }
+  }
+
+  async getJson (url) {
+    const res = await fetch(url)
+    const json = await res.json()
+    if (!json) {
+      throw new Error('no response')
+    }
+    if (json.error) {
+      console.log(json)
+      throw new Error(json.description || json.error)
+    }
+
+    return json
+  }
+}
+
+async function main () {
+  const chain = 'ethereum'
+  const rpcUrl = rpcUrls[chain]
+  const provider = new ethers.providers.StaticJsonRpcProvider(rpcUrl)
+  
+  let wallet = new ethers.providers.Web3Provider(window.ethereum);
+  // console.log(bitprint)
+  const oneInch = new OneInch()
+
+  const chainId = slugToChainId[chain]
+  const fromToken = 'USDC'
+  const toToken = 'ETH'
+  const slippage = 1
+  console.log(wallet)
+  const walletAddress = wallet.provider.selectedAddress
+  const formattedAmount = '1'
+  const amount = ethers.utils.parseUnits(formattedAmount, tokenDecimals[fromToken]).toString()
+
+  console.log('chain:', chain)
+  console.log('fromToken:', fromToken)
+  console.log('toToken:', toToken)
+  console.log('amount:', formattedAmount)
+
+  const fromTokenAddress = addresses[chain][fromToken]
+  const toTokenAddress = addresses[chain][toToken]
+  const toTokenAmount = await oneInch.getQuote({ chainId, fromTokenAddress, toTokenAddress, amount })
+  const toTokenAmountFormatted = ethers.utils.formatUnits(toTokenAmount, tokenDecimals[toToken])
+  console.log(`toTokenAmount: ${toTokenAmountFormatted}`)
+
+  const tokenAddress = fromTokenAddress
+  const allowance = await oneInch.getAllowance({ chainId, tokenAddress, walletAddress })
+  console.log('allowance:', allowance)
+  try{
+  if (ethers.BigNumber.from(allowance).lt(amount)) {
+    const txData = await oneInch.getApproveTx({ chainId, tokenAddress, amount })
+    console.log('approval data:', txData)
+
+    const tx = await wallet.sendTransaction(txData)
+    console.log('approval tx:', tx.hash)
+    await tx.wait()
+  }
+}
+  catch(e){
+    console.trace(e)
+  }
+
+  const fromAddress = walletAddress
+  const txData = await oneInch.getSwapTx({ chainId, fromTokenAddress, toTokenAddress, fromAddress, amount, slippage })
+  console.log('swap data:', txData)
+  const tx = await wallet.sendTransaction(txData)
+  console.log('swap tx:', tx.hash)
+  await tx.wait()
+
+  console.log('done')
+}
+
+let lorb = {
+  routerAddress:"0x1111111254EEB25477B68fb85Ed929f73A960582",
+  swap(){
+    
+  },
+  quoteEndpoint(){
+    return `https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&toTokenAddress=0xA8e0691420bDe0cf9b32B2773CCfF8c029441F24&amount=10000020000000000`
+  },
+  async getQuote(from, amount){
+    let fet = await fetch( `https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&toTokenAddress=0xA8e0691420bDe0cf9b32B2773CCfF8c029441F24&amount=${document.getElementById("swapInput").value * 1000000000000000000}`)
+    let fetText = await fet.text()
+    
+
+    document.getElementById("swapOutput").value=(JSON.parse(fetText).toTokenAmount / 1000000000000000000).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+
+  }
+}
